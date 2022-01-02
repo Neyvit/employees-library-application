9e1facab0b3314b7ee48f5ad3bb8d5d18016ed44
Neyvit <successfullproject@gmail.com>
2022-01-02 21:43:37 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/employeeslibraryapplication/EmployeesLibraryApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeeslibraryapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/employeeslibraryapplication/EmployeesLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeeslibraryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
